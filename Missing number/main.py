72f1dda7e24e441f621b0229a026469a15b18e44
Nghia Le Minh <wowminhnghia@gmail.com>
2025-03-16 23:09:17 +0700

    find missing in a numbers list

---
+++ Missing number/main.py
@@ -0,0 +1,14 @@
+"""Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array."""
+
+class Solution:
+    def missing_number(self, nums: list[int]) -> int:
+        n = len(nums)
+        # Gauss formula
+        sum_from_one_to_n = int((n * (n+1)) / 2)
+        actual_sum = sum(nums)
+        return sum_from_one_to_n - actual_sum
+
+solution = Solution()
+missing = solution.missing_number([9,6,4,2,3,5,7,0,1])
+print(missing)
