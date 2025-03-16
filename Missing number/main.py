263b1841cea11827524f42ac2394ad8b32ccfb24
Nghia Le Minh <wowminhnghia@gmail.com>
2025-03-16 23:10:44 +0700

    add time and space complexity for this problem

--- Missing number/main.py
+++ Missing number/main.py
@@ -1,6 +1,8 @@
 """Given an array nums containing n distinct numbers in the range [0, n],
 return the only number in the range that is missing from the array."""
 
+# Time complexity: O(n)
+# Space complexity: O(1)
 class Solution:
     def missing_number(self, nums: list[int]) -> int:
         n = len(nums)
