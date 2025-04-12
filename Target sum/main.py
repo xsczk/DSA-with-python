53593c8187a8d03aba3ef2e2fea216695340b7c8
Nghia Le Minh <wowminhnghia@gmail.com>
2025-04-13 01:20:46 +0700

    remove redunant else statement

--- Target sum/main.py
+++ Target sum/main.py
@@ -36,33 +36,33 @@ class Solution:
         if current_index == len(nums):
             # Check if the current sum matches the target
             return 1 if current_sum == target else 0
-        else:
-            # Check if the result is already computed
-            if memo[current_index][current_sum + self.total_sum] != float('-inf'):
-                return memo[current_index][current_sum + self.total_sum]
 
-            # Calculate ways by adding the current number
-            add = self.calculate_ways(
-                nums,
-                current_index + 1,
-                current_sum + nums[current_index],
-                target,
-                memo,
-            )
-
-            # Calculate ways by subtracting the current number
-            subtract = self.calculate_ways(
-                nums,
-                current_index + 1,
-                current_sum - nums[current_index],
-                target,
-                memo,
-            )
-
-            # Store the result in memoization table
-            memo[current_index][current_sum + self.total_sum] = add + subtract
-
+        # Check if the result is already computed
+        if memo[current_index][current_sum + self.total_sum] != float('-inf'):
             return memo[current_index][current_sum + self.total_sum]
+
+        # Calculate ways by adding the current number
+        add = self.calculate_ways(
+            nums,
+            current_index + 1,
+            current_sum + nums[current_index],
+            target,
+            memo,
+        )
+
+        # Calculate ways by subtracting the current number
+        subtract = self.calculate_ways(
+            nums,
+            current_index + 1,
+            current_sum - nums[current_index],
+            target,
+            memo,
+        )
+
+        # Store the result in memoization table
+        memo[current_index][current_sum + self.total_sum] = add + subtract
+
+        return memo[current_index][current_sum + self.total_sum]
 
 
 solution = Solution()
