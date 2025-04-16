abeca8d0a99505f79ce6afe52e9187f64eb71c51
Nghia Le Minh <wowminhnghia@gmail.com>
2025-04-16 23:27:33 +0700

    reverse linked list by using recursion approach

--- Reversed linked list/main.py
+++ Reversed linked list/main.py
@@ -13,6 +13,7 @@ class ListNode:
 # Time complexity: O(n)
 # Space complexity: O(1)
 class Solution:
+    # iteratively
     def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
         prev = None
@@ -27,4 +28,10 @@ class Solution:
         # prev now is the head of the reverse linked list
         return prev
 
-
+    # recursively
+    def reverse_list_recursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head;
+        new_head = self.reverse_list_recursive(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
