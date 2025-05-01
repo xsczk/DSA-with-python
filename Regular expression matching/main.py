2cd6f2854540bbc7311642aa194714f498eded8b
Nghia Le Minh <wowminhnghia@gmail.com>
2025-05-01 17:52:11 +0700

    implement with top-down dynamic programing

--- Regular expression matching/main.py
+++ Regular expression matching/main.py
@@ -20,6 +20,8 @@ Explanation: ".*" means "zero or more (*) of any character (.)".
 
 class Solution:
     # recursion
+    # Time complexity: O(2^(lenS + lenP))
+    # Space complexity: O(lenS * lenP) where lenS and lenP are the len of s and p respectively
     def is_match(self, s: str, p: str) -> bool:
         """
         :param s: text
@@ -39,7 +41,32 @@ class Solution:
             # if first match and the next pattern is not *, continue to check remaining of both
             return first_match and self.is_match(s[1:], p[1:])
 
+    # top-down dynamic programing with memoization based on recursion approach above
+    # Time complexity: O(lenS * lenP)
+    # Space complexity: O(lenS * lenP)
+    def is_match_dp(self, s: str, p: str) -> bool:
+        dp = {}
+
+        def dfs(i: int, j: int) -> bool:
+            if (i, j) in dp: return dp[(i, j)]
+            # if i and j is out of bound, the initial string matches the pattern
+            if i >= len(s) and j >= len(p):
+                return True
+            # if j is out of bound but i doesn't => the string doesn't match the pattern
+            if j >= len(p): return False
+            first_match = i < len(s) and p[j] in [s[i], '.']
+            if j + 1 < len(p) and p[j + 1] == '*':
+                # case 1: don't use the *
+                # case 2: if first char is match, we can continue to check the remaining of string s
+                # with pattern p
+                dp[(i, j)] = dfs(i, j + 2) or (first_match and dfs(i + 1, j))
+            else:
+                dp[(i, j)] = first_match and dfs(i + 1, j + 1)
+            return dp[(i, j)]
+
+        return dfs(0, 0)
+
 
 solution = Solution()
-match = solution.is_match("aa", "a*")
+match = solution.is_match_dp("aa", "a*")
 print(match)
