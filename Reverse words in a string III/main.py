9ce8b7b344793185e0d652d41fabfd5a0bf61d1e
Nghia Le Minh <wowminhnghia@gmail.com>
2025-05-27 23:26:26 +0700

    reverse words in a string while still preserving whitespace and initial word order

---
+++ Reverse words in a string III/main.py
@@ -0,0 +1,39 @@
+"""Given a string s, reverse the order of characters in each word within a sentence
+while still preserving whitespace and initial word order.
+
+Constraints:
+- 1 <= s.length <= 5 * 104
+- s contains printable ASCII characters.
+- s does not contain any leading or trailing spaces.
+- There is at least one word in s.
+- All the words in s are separated by a single space.
+"""
+
+
+class Solution:
+    # iterative and reverse each word
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def reverse_words(self, s: str) -> str:
+        words = s.split(" ")
+        reversed_words = [word[::-1] for word in words]
+        return ' '.join(reversed_words)
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def reverse_words_2(self, s: str) -> str:
+        last_space_index = -1
+        result = []
+        for str_index in range(len(s)):
+            # if we are currently at the last of the s or current character is a space
+            if s[str_index] == ' ' or str_index == len(s) - 1:
+                last_character = str_index - 1 if s[str_index] == ' ' else str_index
+                # reverse each word
+                for c_index in range(last_character, last_space_index, -1):
+                    result += s[c_index]
+                if str_index != len(s) - 1:
+                    result += ' '
+                # assign last_space_index to str_index since the first index of the character
+                # of the next word will be last_space_index + 1 for the next iteration
+                last_space_index = str_index
+        return ''.join(result)
