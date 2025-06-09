97520a539544c04dd7e07238905b75c0647cd140
Nghia Le Minh <wowminhnghia@gmail.com>
2025-06-09 23:04:59 +0700

    using dynamic programing approach and apply on typescript

---
+++ Palindromic substrings/main.ts
@@ -0,0 +1,40 @@
+/**
+ * Given a string s, return the number of palindromic substrings in it.
+ *
+ * A string is a palindrome when it reads the same backward as forward.
+ *
+ * A substring is a contiguous sequence of characters within the string.
+ *
+ * Constraints:
+ * - 1 <= s.length <= 1000
+ * - s consists of lowercase English letters.
+ */
+
+// dynamic programing, memoization
+// time complexity: O(n^2)
+// space complexity: O(n^2) due to 2D-dp list
+function countSubstrings(s: string): number {
+   let ans = 0
+   if (!s) return ans
+   const n = s.length
+   const dp = Array.from({length: n}, () => Array(n).fill(false))
+   // every single character is a palindrome
+   for (let i = 0; i < n; i++) {
+      dp[i][i] = true
+      ans++
+   }
+   // check for substrings that have length >= 2
+   for (let length = 2; length <= n; length++) {
+      for (let start = 0; start <= n - length; start++) {
+         const end = start + length - 1
+         // if abc is a palindrome and x = y => xabcy is also a palindrome
+         if (s[start] == s[end] && (length == 2 || dp[start + 1][end - 1])) {
+            dp[start][end] = true
+            ans++
+         }
+      }
+   }
+   return ans
+}
+
+countSubstrings('abc')
