e7ae33983b54c06ea9d9aadf78dc73c6dd9843e6
Nghia Le Minh <wowminhnghia@gmail.com>
2025-07-21 23:40:12 +0700

    check whether typed is match with name

---
+++ Long pressed name/main.py
@@ -0,0 +1,43 @@
+"""
+Your friend is typing his name into a keyboard. Sometimes, when typing a character c,
+the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that
+it was your friends name, with some characters (possibly none) being long pressed.
+
+Constraints:
+- 1 <= name.length, typed.length <= 1000
+- name and typed consist of only lowercase English letters.
+"""
+
+
+class Solution:
+    # two pointers
+    # time complexity: O(n)
+    # space complexity: O(1)
+    def is_long_pressed_name(self, name: str, typed: str) -> bool:
+        np, tp = 0, 0
+        # advance two pointers, until we exhaust one of the strings
+        while np < len(name) and tp < len(typed):
+            # if there's a match, move both pointers forward
+            if name[np] == typed[tp]:
+                np += 1
+                tp += 1
+            # if there's a mismatch but the current typed is similar to its the previous, we can skip
+            elif tp >= 1 and typed[tp] == typed[tp - 1]:
+                tp += 1
+            else:
+                return False
+        # if there's still some characters left *unmatched* in the origin string,
+        # then we don't have a match.
+        # e.g.  name = "alexd"  typed = "ale"
+        if np != len(name):
+            return False
+        # in the case that there're some redundant characters left in typed
+        # we could still have a match.
+        # e.g.  name = "abc"  typed = "abccccc"
+        while tp < len(typed):
+            if typed[tp] != typed[tp - 1]:
+                return False
+            tp += 1
+        return True
