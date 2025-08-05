f07aa3ca27cef3de5fdabb30adb2cd20187901f5
Nghia Le Minh <wowminhnghia@gmail.com>
2025-08-05 23:08:10 +0700

    flatten the binary tree to linked list (stack + recursion)

---
+++ Flatten Binary tree to Linked list/main.py
@@ -0,0 +1,55 @@
+"""
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+- The "linked list" should use the same TreeNode class
+where the right child pointer points to the next node in the list
+and the left child pointer is always null.
+- The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+Constraints:
+- The number of nodes in the tree is in the range [0, 2000].
+- -100 <= Node.val <= 100
+"""
+
+"""
+                1
+              /   \
+             2     5     =>  1 -> 2 -> 3 -> 4 -> 5 -> 6
+            / \     \
+           3   4     6
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pre_order_traversal(self, node: Optional[TreeNode],
+                            stack: list[Optional[TreeNode]]) -> None:
+        if node is not None:
+            self.pre_order_traversal(node.right, stack)
+            self.pre_order_traversal(node.left, stack)
+            stack.append(node)
+
+    # stack, recursion
+    # time complexity: O(n)
+    # space complexity: O(n)
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        stack: list[Optional[TreeNode]] = []
+        if root:
+            self.pre_order_traversal(root, stack)
+        while stack:
+            head = stack.pop()
+            # left child pointer is always null
+            head.left = None
+            # right child pointer points to the next node in the list
+            head.right = None if len(stack) - 1 < 0 else stack[-1]
