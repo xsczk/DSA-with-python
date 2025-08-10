e05ef00f706e333bce4398d0aa7d8bc73c8ef9ee
Nghia Le Minh <wowminhnghia@gmail.com>
2025-08-10 11:32:50 +0700

    find the length of the longest well-formed parentheses substring

---
+++ Longest valid parentheses/main.py
@@ -0,0 +1,34 @@
+"""
+Given a string containing just the characters '(' and ')', return the length
+of the longest valid (well-formed) parentheses substring.
+A substring is a contiguous non-empty sequence of characters within a string.
+
+Constraints:
+- 0 <= s.length <= 3 * 10^4
+- s[i] is '(', or ')'.
+"""
+
+
+class Solution:
+    # stack
+    # time complexity: O(n)
+    # space complexity: O(n)
+    def longest_valid_parentheses(self, s: str) -> int:
+        max_len = 0
+        stack = []
+        # the position of the last invalid character which can't form a valid group of parentheses
+        last_invalid = -1
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif stack:
+                # found a valid open bracket
+                stack.pop()
+                # calculating the start position of the valid group
+                start = stack[-1] + 1 if stack else last_invalid + 1
+                max_len = max(max_len, i - start + 1)
+            else:
+                # update the stack to init state and last_invalid to i as we currently at the invalid position
+                stack = []
+                last_invalid = i
+        return max_len
