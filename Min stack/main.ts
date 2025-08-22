dc2ccd6cd384452fd3797aa2ab8f5cd51493b807
Nghia Le Minh <wowminhnghia@gmail.com>
2025-08-22 22:43:59 +0700

    implement a min stack using typescript

---
+++ Min stack/main.ts
@@ -0,0 +1,45 @@
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ * Implement the MinStack class:
+ *
+ * - MinStack() initializes the stack object.
+ * - void push(int val) pushes the element val onto the stack.
+ * - void pop() removes the element on the top of the stack.
+ * - int top() gets the top element of the stack.
+ * - int getMin() retrieves the minimum element in the stack.
+ * You must implement a solution with O(1) time complexity for each function.
+ *
+ * Constraints:
+ * - -2^31 <= val <= 2^31 - 1
+ * - Methods pop, top and getMin operations will always be called on non-empty stacks.
+ * - At most 3 * 104 calls will be made to push, pop, top, and getMin.
+ */
+
+class MinStack {
+   st: number[][]
+
+   constructor() {
+      this.st = []
+   }
+
+   push(val: number): void {
+      let current_min = this.getMin()
+      if (current_min === undefined || current_min > val) {
+         current_min = val
+      }
+      this.st.push([val, current_min])
+   }
+
+   pop(): void {
+      this.st.pop()
+   }
+
+   top(): number | undefined {
+      return this.st.at(-1)?.[0]
+   }
+
+   getMin(): number | undefined {
+      return this.st.at(-1)?.[1]
+   }
+}
